a9215ae5c9f72ac585cab68c3e6692357c964538
Ouro <ourosalimabdul@gmail.com>
2020-03-04 18:08:29 -0500

    Added JAWSDB conection to connection.js

--- Config/connection.js
+++ Config/connection.js
@@ -1,13 +1,22 @@
 var mysql = require("mysql");
 var passWord = require("../passWord");
+var connection;
 //console.log("Making connection");
-var connection = mysql.createConnection({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: passWord,
-    database: "burgers_db"
+if(process.env.JAWSDB_URL)
+{
+  connection = mysql.createConnection(process.env.JAWSDB_URL)
+}
+else
+{ 
+  connection = mysql.createConnection(
+    {
+      host: "localhost",
+      port: 3306,
+      user: "root",
+      password: passWord,
+      database: "burgers_db"
   });
+}
   
   connection.connect(function(err) {
     if (err) {
